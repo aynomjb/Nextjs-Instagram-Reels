ea9b91a0dc0ce408a763b0532b1f23d945565ea3
Jadavananda Bordoloi <jb985953@gmail.com>
2025-12-27 10:46:41 +0530

    Added reel item and passed the reels

---
+++ app/reels/ReelItem.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+/* ---------------- Reel Item ---------------- */
+export default function ReelItem({
+  src,
+  reelId = "demo-id",
+  username = "anishadev",
+  caption = "Building Instagram Reels UI in Next.js 🔥",
+  music = "Original Audio • anishadev",
+}: {
+  src: string;
+  reelId?: string;
+  username?: string;
+  caption?: string;
+  music?: string;
+}) {
+  return (<></>)
+}
